edbb15ff52a3c4e2b6f830aef3048a119f34d4ca
Willem Drescher <willemdrescher@yahoo.com>
2020-09-22 12:17:31 -0400

    add digest auth

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,18 @@
 from flask import Flask, jsonify
+from flask_httpauth import HTTPDigestAuth
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+auth = HTTPDigestAuth()
+
+@auth.get_password
+def get_pw(username):
+    if username == 'vcu':
+        return "rams"
+    return None
 
 @app.route("/", methods=['GET'])
+@auth.login_required
 def get():
         return jsonify()
 
