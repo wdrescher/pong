7d05536f9a5206fbd38f1589c8cb6ac68e8f1980
Willem Drescher <willemdrescher@yahoo.com>
2020-09-22 11:24:15 -0400

    refactor code

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from app.main import app
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def get(self):
+        return jsonify()
 
 if __name__ == "__main__": 
     app.run()
